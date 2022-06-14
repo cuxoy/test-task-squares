2ee9d00b6ff4f6cfedcf780603c69fe620c31f63
Dmytro <ccccuxoy@gmail.com>
2022-06-14 21:47:36 +0300

    test commit

---
+++ src/classComponents/classSquareItem/ClassSquareItem.js
@@ -0,0 +1,24 @@
+import { useDispatch, useSelector } from "react-redux";
+import { formVisible } from "../../actions/actions";
+import "./style.scss";
+
+const SquareItem = ({ id, color, text }) => {
+
+  const dispatch = useDispatch();
+
+  const onFormOpen = () => {
+    dispatch(formVisible(id));
+  };
+  
+  return (
+    <div
+      key={id}
+      className="square-item"
+      style={{ "background-color": `${color}` }}
+      onClick={onFormOpen}
+    >
+      {text}
+    </div>
+  );
+};
+export default SquareItem;
